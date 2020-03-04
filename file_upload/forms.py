387e7f0b01fcd2e30ab874a27838c895f188d34c
antonella.caroca <antonella.caroca@cci-entel.cl>
2020-03-04 16:31:54 -0300

    proyecto terminado

--- file_upload/forms.py
+++ file_upload/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from .models import Document
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class DocumentForm(forms.ModelForm):
+    date = forms.DateField(widget=DateInput)
     class Meta:
         model = Document
         fields = ('title', 'date', 'pdf', 'cover')
+        widgets = {'date' : DateInput}
--- file_upload/models.py
+++ file_upload/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Document(models.Model):
-    title = models.CharField(max_length=100)
-    date = models.DateField()
+    title = models.CharField("Título", max_length=100)
+    date = models.DateField("Fecha")
     pdf = models.FileField(upload_to='docs/pdfs')
-    cover = models.ImageField(upload_to='docs/covers', null='True', blank='True')
+    cover = models.ImageField("Foto", upload_to='docs/covers', null='True', blank='True')
 
     def __str__(self):
         return self.title
